5b262316447179576ca28805cd98219306a4b945
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-05 01:36:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,7 @@ def square_array(array)
   # your code here
   square_array=[]
   array.length.times.do |index|
-    square_array.push(array|index|**2)
+    square_array.push(array|index|(**2))
   end
+  square_array
 end
