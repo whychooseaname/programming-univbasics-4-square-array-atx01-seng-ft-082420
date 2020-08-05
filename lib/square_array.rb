26d95bc36bf3e340ddc78caf8d48a8bb95e62db0
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-05 02:02:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
 counter=0
  new_array = []
 
- while counter<array.length do |integer|
-  square_array << array |integer| ** 2
+ while counter<array.length do {|integer|
+  square_array << array |integer| ** 2}
   counter+=1
  end
  return square_array
