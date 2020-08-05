6c7552611f214e59c34c26d3e01cf7b3144cc2ac
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-05 01:39:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,9 @@ def square_array(array)
   # your code here
   counter=0
   square_array=[]
-  array.length.times.do 
+ while counter <array.length do 
     square_array.push(array**2)
+    counter+=1
   end
   square_array
 end
