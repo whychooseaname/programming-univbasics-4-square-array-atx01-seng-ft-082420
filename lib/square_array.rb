9d1393ccb4859702fbd159c09b24e1a0ab131890
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-05 01:40:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter=0
   square_array=[]
  while counter <array.length do 
-    square_array.push(array**2)
+    square_array.push(array(**2))
     counter+=1
   end
   square_array
